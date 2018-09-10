b7e65164f9736f885e537dc42f0403c6c6b83302
Damon <damonbd@gmail.com>
2018-09-10 14:21:53 +0000

    uploadImage partial, cors issue

--- main.go
+++ main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
+	"os"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
@@ -18,25 +21,35 @@ func init() {
 
 func main() {
 	addRoutes()
-	log.Fatal(http.ListenAndServe(":8080", router))
+
+	headersOk := handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type"})
+	originsOk := handlers.AllowedOrigins([]string{os.Getenv("ORIGIN_ALLOWED")})
+	methodsOk := handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "OPTIONS"})
+
+	log.Fatal(http.ListenAndServe(":8080", handlers.CORS(originsOk, headersOk, methodsOk)(router)))
 }
 
 func addRoutes() {
 	router = mux.NewRouter()
 	router.HandleFunc("/", Home).Methods("GET")
-	router.HandleFunc("/jsontest", jsonTest).Methods("GET", "OPTIONS")
+	router.HandleFunc("/uploadImage", uploadImage).Methods("POST", "OPTIONS")
 }
 
 //Home loads React App
 func Home(w http.ResponseWriter, r *http.Request) {
-	tpl.ExecuteTemplate(w, "Home.html", nil)
+	tpl.ExecuteTemplate(w, "home.html", nil)
 }
 
-func jsonTest(w http.ResponseWriter, r *http.Request) {
+func uploadImage(w http.ResponseWriter, r *http.Request) {
+	//interrogate r
+	fmt.Println("cat")
 
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-
-	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-
+	setHeaders(w, r)
 	json.NewEncoder(w).Encode("OK")
+}
+
+func setHeaders(w http.ResponseWriter, r *http.Request) {
+	//w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Content-Type", "application/json")
+	//w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 }
